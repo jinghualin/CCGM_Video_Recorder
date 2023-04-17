f0972ac6cb21cb22f1ebd2062b2b053b89ab4d50
Jinghua Lin <jinghualin@hotmail.com>
2023-04-17 18:43:05 +0200

    add python dependency via poetry

--- src/modules/model/preach_model.py
+++ src/modules/model/preach_model.py
@@ -3,6 +3,7 @@ from pathlib import Path
 from typing import Optional
 from PySide6.QtCore import QObject, Property, Signal
 
+
 class Preach(QObject):
     """Class prividing data to QML UI about preach info and preacher"""
 
@@ -10,20 +11,19 @@ class Preach(QObject):
         """Constructor"""
         super().__init__(parent)
         self._preach_date = None
-        self._preach_title:str=None
-        self._bible:str = None
-        self._preacher_name :str = None
-        self._head_description:str = None
+        self._preach_title: str = None
+        self._bible: str = None
+        self._preacher_name: str = None
+        self._head_description: str = None
 
-    
     @Property(str)
     def preach_date(self):
         return self._preach_date
 
     @preach_date.setter
-    def preach_date(self, date)-> None:
+    def preach_date(self, date) -> None:
         self._preach_date = date
-    
+
     @Property(str)
     def tail_description(self):
         return """
@@ -32,4 +32,3 @@ class Preach(QObject):
         {self._preacher_name}\n\n
         更多讲道和信息请关注官方主页：\n
         www.ccgm-mannheim.de"""
-
